2dc5ae04dbef28ad2372f635c78970005706bc2c
Damian Czarnota <godevdamian@gmail.com>
2020-12-03 10:53:19 +0100

    fix base endpoint url

--- front/constants.js
+++ front/constants.js
@@ -1,3 +1,2 @@
-// export const ENDPOINT_URL = "https://online-gaming-shop.herokuapp.com";
-export const ENDPOINT_URL = "http://localhost:8080";
+export const ENDPOINT_URL = "https://online-gaming-shop.herokuapp.com";
 export const CHECK_IF_EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
